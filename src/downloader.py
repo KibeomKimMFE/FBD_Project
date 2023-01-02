844cb14f90d39cf67026222afa6ff16a6ff4f4b0
qraft-kibeom <kibeom.kim@qraftec.com>
2023-01-03 01:50:35 +0900

    repo initialization

---
+++ src/downloader.py
@@ -0,0 +1,14 @@
+from tardis_dev import datasets
+
+
+if __name__ == "__main__":
+    # we download crypto futures data for the period
+    # 2nd September 2022 to 31st December 2022. (only allowed for academic purposes)
+    datasets.download(
+        exchange="binance-futures",
+        data_types=["trades", "quotes", "book_snapshot_5"],
+        from_date="2022-09-02",
+        to_date="2022-12-31",
+        symbols=["adausdt"],
+        api_key="",  # this info needs to be included to use this package.
+    )
