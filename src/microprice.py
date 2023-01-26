5f3dc11e24eac8d335b83d1733e0d4869d90c33e
qraft-kibeom <kibeom.kim@qraftec.com>
2023-01-26 16:10:14 +0100

    code refactoring

---
+++ src/microprice.py
@@ -0,0 +1,121 @@
+import numpy as np
+import pandas as pd
+
+from numpy.linalg import inv
+from scipy.linalg import block_diag
+
+
+def get_micro_adjustment(df_sig: pd.DataFrame, power: int = 6) -> pd.DataFrame:
+    """_summary_
+
+    Args:
+        df_sig (pd.DataFrame): a symmetrized pandas dataframe that contain discretized
+                                spread, imbalance and mid price.
+        power (int): matrix power of B for microprice adjustment.
+
+    Returns:
+        pd.DataFrame: _description_
+    """
+    nSpread, nImbalance = len(df_sig.ba_spread.unique()), len(df_sig.imbalance.unique())
+    nCombination = nSpread * nImbalance
+
+    # divide datafrmae into two events (dM equals to 0 or not equal to 0)
+    mid_zero, mid_non_zero = (
+        df_sig[df_sig["mid_chg"] == 0],
+        df_sig[df_sig["mid_chg"] != 0],
+    )
+
+    # transition matrix Q
+    mid_zero = mid_zero.groupby(["ba_spread", "imbalance", "next_imbalance"])[
+        "mid_price"
+    ].count()
+    Q_cnt = pd.DataFrame(
+        0,
+        index=pd.MultiIndex.from_product(
+            [
+                list(range(1, nSpread + 1)),
+                list(range(1, nImbalance + 1)),
+                list(range(1, nImbalance + 1)),
+            ],
+            names=["ba_spread", "imbalance", "next_imbalance"],
+        ),
+        columns=["cnt"],
+    )
+    Q_cnt.loc[mid_zero.index] = mid_zero.values.reshape(-1, 1)
+    Q_cnt = block_diag(
+        Q_cnt.loc[1].values.reshape(nSpread, nImbalance),
+        Q_cnt.loc[2].values.reshape(nSpread, nImbalance),
+        Q_cnt.loc[3].values.reshape(nSpread, nImbalance),
+        Q_cnt.loc[4].values.reshape(nSpread, nImbalance),
+    )
+
+    # absorbing state matrix R
+    R = (
+        mid_non_zero.groupby(["ba_spread", "imbalance", "mid_chg"])
+        .count()
+        .unstack("mid_chg")["mid_price"]
+        .fillna(0)
+    )
+    ordering = R.index
+
+    # K contains all the non-zero mid price chg.
+    K = R.columns.values.reshape(-1, 1)
+
+    R_cnt = pd.DataFrame(
+        0,
+        index=pd.MultiIndex.from_product(
+            [list(range(1, nSpread + 1)), list(range(1, nImbalance + 1))],
+            names=["ba_spread", "imbalance"],
+        ),
+        columns=R.columns.values,
+    )
+    R_cnt.loc[R.index] = R.values
+    R_cnt = R_cnt.values
+
+    # get transition prob matrix (transient, absorbing state)
+    J = np.concatenate([Q_cnt, R_cnt], axis=1)
+    J = np.nan_to_num(J / J.sum(axis=1).reshape(-1, 1), nan=0)
+
+    # split Q, R and define I
+    Q, R, I = J[:, :nCombination], J[:, nCombination:], np.eye(nCombination)
+
+    # 1st order micro-price adjustment
+    g1 = inv(I - Q) @ R @ K
+
+    # define new absorbing state
+    T_cnt = pd.DataFrame(
+        0,
+        index=pd.MultiIndex.from_product(
+            [list(range(1, nSpread + 1)), list(range(1, nImbalance + 1))],
+            names=["ba_spread", "imbalance"],
+        ),
+        columns=pd.MultiIndex.from_product(
+            [list(range(1, nSpread + 1)), list(range(1, nImbalance + 1))],
+            names=["next_ba_spread", "next_imbalance"],
+        ),
+    )
+    T = mid_non_zero.pivot_table(
+        index=["ba_spread", "imbalance"],
+        columns=["next_ba_spread", "next_imbalance"],
+        values="mid_price",
+        fill_value=0,
+        aggfunc="count",
+    )
+    T_cnt.loc[T.index, T.columns] = T.values
+    T_cnt = T_cnt.values
+
+    # calculate new trans. prob matrix
+    J2 = np.concatenate([Q_cnt, T_cnt], axis=1)
+    J2 = np.nan_to_num(J2 / J2.sum(axis=1).reshape(-1, 1), nan=0)
+
+    # new Q and T
+    Q, T = J2[:, :nCombination], J2[:, nCombination:]
+
+    # calculate B matrix
+    B = inv(I - Q) @ T
+
+    # calculate the micro price adjustment (g_star) based on
+    # the matrix power input. (default = 6)
+    micro_adj = g1 + np.linalg.matrix_power(B, power) @ g1
+    df = pd.DataFrame(micro_adj, index=ordering, columns=["g_star"])
+    return df
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,11 +1,22 @@
+import dask
 import numpy as np
 import pandas as pd
 
-from numpy.linalg import inv
-from scipy.linalg import block_diag
 
-
+@dask.delayed
 def extract_features(orderbook_file_path: str) -> pd.DataFrame:
+    """
+    Given the orderbook file path, this function extracts
+    key information for computing micro price predictions.
+    First, it calculates bid-ask spreaad, mid price, bid-ask imbalance.
+    Note that this function resamples above quantities by 1 second frequency.
+
+    Args:
+        orderbook_file_path (str): orderbook_file path
+
+    Returns:
+        pd.DataFrame: a dataframe containing necessary quantities.
+    """
     df = pd.read_csv(orderbook_file_path)[
         [
             "timestamp",
@@ -22,6 +33,7 @@ def extract_features(orderbook_file_path: str) -> pd.DataFrame:
     df["imbalance"] = df["bids[0].amount"] / (
         df["bids[0].amount"] + df["asks[0].amount"]
     )
+
     df["timestamp"] = pd.to_datetime(df["timestamp"] / 1000, unit="ms")
 
     # convert timestamp to datetime format
@@ -32,8 +44,28 @@ def extract_features(orderbook_file_path: str) -> pd.DataFrame:
     return df
 
 
+@dask.delayed
+def extract_quotes(trade_file_path: str) -> pd.DataFrame:
+    df = pd.read_csv(trade_file_path)[
+        ["timestamp", "ask_price", "bid_price", "ask_amount", "bid_amount"]
+    ]
+    df["timestamp"] = pd.to_datetime(df["timestamp"] / 1000, unit="ms")
+    return df.set_index("timestamp")
+
+
+@dask.delayed
+def extract_trades(trade_file_path: str) -> pd.DataFrame:
+    df = pd.read_csv(trade_file_path)[["timestamp", "side", "price", "amount"]]
+    df["timestamp"] = pd.to_datetime(df["timestamp"] / 1000, unit="ms")
+    return df.set_index("timestamp")
+
+
 def symmetrize_data(
-    df_feature: pd.DataFrame, numSpreads: int = 4, numImbalance: int = 4, numdM: int = 2
+    df_feature: pd.DataFrame,
+    numSpreads: int = 4,
+    numImbalance: int = 4,
+    numdM: int = 2,
+    symmetrize: bool = True,
 ) -> pd.DataFrame:
     """_summary_
 
@@ -41,11 +73,12 @@ def symmetrize_data(
         df_feature (pd.DataFrame): _description_
         numSpreads (int, optional): _description_. Defaults to 4.
         numImbalance (int, optional): _description_. Defaults to 4.
+        numdM (int, optional): _description_. Defaults to 2.
+        symmetrize (bool, optional): _description_. Defaults to True.
 
     Returns:
         pd.DataFrame: _description_
     """
-
     df_signal = df_feature.copy(deep=True)
     tick_size = df_signal.ba_spread[df_signal.ba_spread != 0].min()
 
@@ -75,122 +108,16 @@ def symmetrize_data(
     df_signal["next_imbalance"] = df_signal["imbalance"].shift(-1)
     df_signal = df_signal.dropna()
 
-    # make symmetric data
-    df_symmetric = df_signal.copy(deep=True)
-    df_symmetric["imbalance"] = numImbalance - df_signal["imbalance"] + 1
-    df_symmetric["next_imbalance"] = numImbalance - df_signal["next_imbalance"] + 1
-    df_symmetric["mid_chg"] = -df_signal["mid_chg"]
+    if symmetrize:  # make symmetric data
+        df_symmetric = df_signal.copy(deep=True)
+        df_symmetric["imbalance"] = numImbalance - df_signal["imbalance"] + 1
+        df_symmetric["next_imbalance"] = numImbalance - df_signal["next_imbalance"] + 1
+        df_symmetric["mid_chg"] = -df_signal["mid_chg"]
+        df = pd.concat([df_signal, df_symmetric])
+    else:
+        df = df_signal
 
-    df = pd.concat([df_signal, df_symmetric])
     df[["next_ba_spread", "next_imbalance"]] = df[
         ["next_ba_spread", "next_imbalance"]
     ].astype(int)
     return df.dropna()
-
-
-def get_micro_adjustment(df_sig: pd.DataFrame) -> list[np.ndarray, np.ndarray]:
-    """_summary_
-
-    Args:
-        df_sig (pd.DataFrame): _description_
-
-    Returns:
-        list[np.ndarray, np.ndarray]: _description_
-    """
-    nSpread, nImbalance = len(df_sig.ba_spread.unique()), len(df_sig.imbalance.unique())
-    nCombination = nSpread * nImbalance
-
-    # divide datafrmae into two events (dM equals to 0 or not equal to 0)
-    mid_zero, mid_non_zero = (
-        df_sig[df_sig["mid_chg"] == 0],
-        df_sig[df_sig["mid_chg"] != 0],
-    )
-
-    print(mid_zero.shape, mid_non_zero.shape)
-    # transition matrix Q
-    mid_zero = mid_zero.groupby(["ba_spread", "imbalance", "next_imbalance"])[
-        "mid_price"
-    ].count()
-    Q_cnt = pd.DataFrame(
-        0,
-        index=pd.MultiIndex.from_product(
-            [
-                list(range(1, nSpread + 1)),
-                list(range(1, nImbalance + 1)),
-                list(range(1, nImbalance + 1)),
-            ],
-            names=["ba_spread", "imbalance", "next_imbalance"],
-        ),
-        columns=["cnt"],
-    )
-    Q_cnt.loc[mid_zero.index] = mid_zero.values.reshape(-1, 1)
-    Q_cnt = block_diag(
-        Q_cnt.loc[1].values.reshape(nSpread, nImbalance),
-        Q_cnt.loc[2].values.reshape(nSpread, nImbalance),
-        Q_cnt.loc[3].values.reshape(nSpread, nImbalance),
-        Q_cnt.loc[4].values.reshape(nSpread, nImbalance),
-    )
-
-    # absorbing state matrix R
-    R = (
-        mid_non_zero.groupby(["ba_spread", "imbalance", "mid_chg"])
-        .count()
-        .unstack("mid_chg")["mid_price"]
-        .fillna(0)
-    )
-    # K contains all the non-zero mid price chg.
-    K = R.columns.values.reshape(-1, 1)
-
-    R_cnt = pd.DataFrame(
-        0,
-        index=pd.MultiIndex.from_product(
-            [list(range(1, nSpread + 1)), list(range(1, nImbalance + 1))],
-            names=["ba_spread", "imbalance"],
-        ),
-        columns=R.columns.values,
-    )
-    R_cnt.loc[R.index] = R.values
-    R_cnt = R_cnt.values
-
-    # get transition prob matrix (transient, absorbing state)
-    J = np.concatenate([Q_cnt, R_cnt], axis=1)
-    J = np.nan_to_num(J / J.sum(axis=1).reshape(-1, 1), nan=0)
-
-    # split Q, R and define I
-    Q, R, I = J[:, :nCombination], J[:, nCombination:], np.eye(nCombination)
-
-    # 1st order micro-price adjustment
-    g1 = inv(I - Q) @ R @ K
-
-    # define new absorbing state
-    T_cnt = pd.DataFrame(
-        0,
-        index=pd.MultiIndex.from_product(
-            [list(range(1, nSpread + 1)), list(range(1, nImbalance + 1))],
-            names=["ba_spread", "imbalance"],
-        ),
-        columns=pd.MultiIndex.from_product(
-            [list(range(1, nSpread + 1)), list(range(1, nImbalance + 1))],
-            names=["next_ba_spread", "next_imbalance"],
-        ),
-    )
-    T = mid_non_zero.pivot_table(
-        index=["ba_spread", "imbalance"],
-        columns=["next_ba_spread", "next_imbalance"],
-        values="mid_price",
-        fill_value=0,
-        aggfunc="count",
-    )
-    T_cnt.loc[T.index, T.columns] = T.values
-    T_cnt = T_cnt.values
-
-    # calculate new trans. prob matrix
-    J2 = np.concatenate([Q_cnt, T_cnt], axis=1)
-    J2 = np.nan_to_num(J2 / J2.sum(axis=1).reshape(-1, 1), nan=0)
-
-    # new Q and T
-    Q, T = J2[:, :nCombination], J2[:, nCombination:]
-
-    # calculate B matrix
-    B = inv(I - Q) @ T
-    return g1, B
